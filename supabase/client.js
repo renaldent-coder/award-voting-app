582d3e9b02fa2d80d1054041e2ddd3c90ca3d1cb
Renaldent Coder <renaldent&gmail.com>
2026-08-03 13:20:33 +0100

    supabase fix

--- supabase/client.js
+++ supabase/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = process.env.SUPABASE_URL || 'https://qzxrsvapvlkijgkeezsp.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY || 'sb_publishable_HONLI6rtWjXOue12FNkVOw_g3CJd6RA'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
